5031804ed96c97a958a9c95e16df70c7861467f1
unknown <kenionatan@gmail.com>
2018-05-26 16:41:27 -0300

    26-05-2018-16-41

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import { Usuario } from '../../domain/usuario/usuario';
+import { MinhaContaPage } from '../minha-conta/minha-conta';
 
 @Component({
   selector: 'page-login',
@@ -7,7 +10,35 @@ import { NavController } from 'ionic-angular';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController) {
+  public data;
+  public http;
+  public usuario: Usuario;
+  public usuarioLogado: Usuario;
+
+  constructor(public navCtrl: NavController, http: Http) {
+    this.data = {};
+    this.data.response = '';
+    this.http = http;
+    this.usuario = new Usuario(null, null, null, null);
+  }
+
+  submit(){
+    var link = 'http://kenio.top/page/login_ionic';
+    var data = JSON.stringify({email: this.usuario.email, password: this.usuario.password})
+
+    this.http.post(link, data)
+      .subscribe(data => {
+        this.data.response = data._body;
+        if(this.data.response == "sucesso"){
+          //this.usuarioLogado.email = this.usuario.email;
+          //this.usuarioLogado.estatus = "autenticado";
+          this.navCtrl.push(MinhaContaPage);
+        }else if(this.data.response == "invalido"){
+          console.log("Login ou senha inválidos");
+        }
+      },error => {
+        console.log("Ocorreu um erro!");
+      });
   }
   
 }
--- src/pages/minha-conta/minha-conta.ts
+++ src/pages/minha-conta/minha-conta.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { LoginPage } from '../login/login';
+import { CadastroPage } from '../cadastro/cadastro';
+
 @Component({
   selector: 'page-minha-conta',
   templateUrl: 'minha-conta.html'
@@ -8,6 +11,16 @@ import { NavController } from 'ionic-angular';
 export class MinhaContaPage {
 
   constructor(public navCtrl: NavController) {
+  }
+
+  goToCadastro(params){
+    if(!params) params = {};
+    this.navCtrl.setRoot(CadastroPage);
+  }
+
+  goToLogin(params){
+    if(!params) params = {};
+    this.navCtrl.setRoot(LoginPage);
   }
   
 }
